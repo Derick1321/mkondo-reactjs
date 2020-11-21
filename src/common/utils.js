c557aa40f88dbc540d270328e9693ea8222e3e7b
Harvey Kadyanji <harvey.kadyax@gmail.com>
2020-11-22 01:27:02 +0300

    audio player init volume, + improve status...

---
+++ src/common/utils.js
@@ -0,0 +1,13 @@
+import { Duration } from 'luxon';
+
+export const shuffleArray = (array) => {
+  for (let i = array.length - 1; i > 0; i -= 1) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
+}
+
+export const formatTime = (seconds) => {
+  return Duration.fromObject({ seconds }).toFormat('mm:ss');
+}
