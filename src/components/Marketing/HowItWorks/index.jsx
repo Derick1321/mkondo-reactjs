4e34b436e51a9c33f41208c40fdd7349c2076a1b
Harvey Kadyanji <harvey.kadyax@gmail.com>
2020-11-23 14:06:32 +0300

    move outside...

--- src/components/Marketing/HowItWorks/index.jsx
+++ src/components/Marketing/HowItWorks/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import Feature from '$components/common/Feature';
-import List from '../List';
+import List from '$components/marketing/List';
 
 import model from './model';
 
