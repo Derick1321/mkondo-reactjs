a395d3f6fbaf624d4641669227e3f4af399d2dbc
Harvey Kadyanji <harvey.kadyax@gmail.com>
2020-11-23 14:14:10 +0300

    disable how it works temp...

--- src/containers/Marketing/index.jsx
+++ src/containers/Marketing/index.jsx
@@ -7,7 +7,7 @@ import Hero from '$components/common/Hero';
 import Tabs from '$components/common/Tabs';
 import Preview from '$components/common/Preview';
 import TopSongs from '$components/common/TopSongs';
-import HowItWorks from '$components/marketing/HowItWorks';
+//import HowItWorks from '$components/marketing/HowItWorks';
 import AppDownload from '$components/marketing/AppDownload';
 import AlbumMenuPanel from '$components/common/AlbumMenuPanel';
 
@@ -122,7 +122,6 @@ const Marketing = () => {
       </div>
       <div className="row how-it-works-pane">
         <div className="col-12 col-md-10 offset-md-1">
-          <HowItWorks />
         </div>
       </div>
       <div className="row app-download-wrapper justify-content-center align-items-center">
