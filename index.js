c357a09efa203e173f59a2ac3a95bdeccdf33292
Harvey Kadyanji <harvey.kadyax@gmail.com>
2020-11-28 00:14:06 +0300

    RDK bug with parcel...

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 
+import 'regenerator-runtime/runtime'
+
 import store from './src/redux/store';
 import App from './src/containers/App';
 
