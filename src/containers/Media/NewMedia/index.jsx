c06ef1b37cb5b3bcad0b7a23c56be059ea10f600
Harvey Kadyanji <harvey.kadyax@gmail.com>
2020-11-27 23:02:30 +0300

    add browser flag to ensure it builds properly...

--- src/containers/Media/NewMedia/index.jsx
+++ src/containers/Media/NewMedia/index.jsx
@@ -1,16 +1,55 @@
 import React, { useState } from 'react';
 
 import DragDrop from '$components/common/DragDrop';
+import NewItem from '$components/common/NewItem';
 
 import './index.scss';
+
+const menus = [
+  { name: 'title', type: 'text', placeholder: 'Enter Title', title: 'Title' },
+  { name: 'genre', type: 'select', placeholder: 'Enter Genre', title: 'Genre' },
+  { name: 'description', type: 'area', placeholder: 'Describe your track', title: 'Description' },
+  { name: 'caption', type: 'area', placeholder: 'Add your caption', title: 'Caption' },
+];
+
+const metamenus = [
+  { name: 'artist', type: 'text', placeholder: 'Artist Name', title: 'Artist' },
+  { name: 'publisher', type: 'text', placeholder: 'Publisher Name', title: 'Publisher' },
+  { name: 'composer', type: 'text', placeholder: 'Composer Name', title: 'Composer' },
+  { name: 'releaseDate', type: 'date', placeholder: 'Release Date', title: 'Release Date' },
+  { name: 'albumTitle', type: 'text', placeholder: 'Enter Album Title', title: 'Album Title' },
+  { name: 'recordLabel', type: 'text', placeholder: 'Enter Record Label', title: 'Record Label' },
+];
 
 const NewMedia = () => {
   // state
   const [files, setFiles] = useState([]);
+  const [values, setValues] = useState({
+    fullName: '',
+    genre: '',
+    description: '',
+    caption: '',
+    privacy: false,
+  });
 
   // handlers
-  const handleChange = (values) => {
-    setFiles([...files, ...values]);
+  const handleFileChange = (values) => {
+    setFiles([
+      ...files,
+      ...values
+    ]);
+  }
+
+   // handleChange
+  const handleChange = (name, value) => {
+    const payload = {
+      [name]: value,
+    };
+
+    setValues({
+      ...values,
+      payload,
+    });
   }
 
   // render
@@ -19,8 +58,11 @@ const NewMedia = () => {
       <div className="row justify-content-center">
         <div className="col-10 col-sm-8 col-md-6">
           <div className="d-flex flex-column">
-            <DragDrop
+            <NewItem 
+              menus={menus}
+              metamenus={metamenus}
               onChange={handleChange}
+              values={values}
             />
           </div>
         </div>
