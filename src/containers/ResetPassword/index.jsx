0f179c1e11a2884779886c03e9bcf9b6146f080a
Harvey Kadyanji <harvey.kadyax@gmail.com>
2020-12-12 17:16:05 +0300

    reset password complete...

---
+++ src/containers/ResetPassword/index.jsx
@@ -0,0 +1,25 @@
+import React, { useEffect } from 'react';
+import { useLocation, useHistory } from 'react-router-dom';
+import queryString from 'query-string';
+
+import { routePaths } from '$common/routeConfig';
+import { logout } from '$redux/features/authentication';
+
+const ResetPassword = () => {
+  // store
+  const location = useLocation();
+  const history = useHistory();
+
+  // effects
+  useEffect(() => {
+    const search = queryString.parse(location.search);
+    history.replace(routePaths.marketing, { token: search.token });
+  }, []);
+
+  // render
+  return (
+    <div />
+  );
+};
+
+export default ResetPassword;
