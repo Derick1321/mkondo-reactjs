ae17d1712ac3cf06488b257edb675dc71ad7b63e
Harvey Kadyanji <harvey.kadyax@gmail.com>
2021-01-20 13:55:09 +0300

    cover upload + profile fix and responsiveness

--- src/components/common/modals/ShareModal/index.jsx
+++ src/components/common/modals/ShareModal/index.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import Share from '$components/common/Share';
 
@@ -9,9 +10,12 @@ const ShareModal = (props) => {
   const {
     title, 
     country,
-    mediaId,
+    id,
     avatarUrl,
+    isArtist,
   } = props;
+
+  const param = isArtist ? 'artist' : 'media';
 
   // render
   return (
@@ -19,13 +23,27 @@ const ShareModal = (props) => {
       <Share
         name={title}
         country={country}
-        link={`https//mkondo.co/app/media/${mediaId}`}
+        link={`https://mkondo.co/app/${param}/${id}`}
         avatar={avatarUrl}
-        initialDescription="Checkout this media hosted at www.mkondo.co"
-        mediaId={mediaId}
+        initialDescription={`Checkout this ${param} hosted at www.mkondo.co`}
+        id={id}
       />
     </div>
   );
+}
+
+ShareModal.defaultProps  = {
+  isArtist: false,
+  country: '',
+  avatarUrl: '',
+}
+
+ShareModal.propTypes  = {
+  title: PropTypes.string.isRequired, 
+  id: PropTypes.string.isRequired,
+  isArtist: PropTypes.bool,
+  country: PropTypes.string,
+  avatarUrl: PropTypes.string.isRequired,
 }
 
 export default ShareModal;
