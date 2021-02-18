d214bb6de3e0019e4cb02331a4b22311aedbacca
Harvey Kadyanji <harvey.kadyax@gmail.com>
2021-02-18 13:13:37 +0300

    clean up...

--- src/common/requestUtils.js
+++ src/common/requestUtils.js
@@ -1,4 +1,4 @@
-const BASE_URL = 'http://127.0.0.1:5000';
+const BASE_URL = 'https://api.mkondo.co';
 
 // Ensure you are running a local instance
 const URL = BASE_URL; // process.env.ENV === 'development' ? 'http://localhost:5000' : BASE_URL;
