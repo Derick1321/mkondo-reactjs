8fc03d1317a072340995eb1aad0cad6fe322db21
Harvey Kadyanji <harvey.kadyax@gmail.com>
2021-03-10 21:33:43 +0300

    scroll to not working--

--- src/components/common/ScrollPanel/index.jsx
+++ src/components/common/ScrollPanel/index.jsx
@@ -1,77 +1,88 @@
-import React, { useState, useRef } from 'react';
+import React, { useCallback, useRef, useEffect } from 'react';
 import PropTypes from 'prop-types';
-import { useHistory, generatePath } from 'react-router-dom';
-import { useDispatch } from 'react-redux';
-import ScrollMenu from 'react-horizontal-scrolling-menu';
+import gsap from 'gsap';
+import { Draggable } from 'gsap/Draggable';
+import { ScrollToPlugin } from 'gsap/ScrollToPlugin';
 
-import AlbumMenu from '$components/common/AlbumMenu';
 import Button from '$components/common/Button';
-
-import { routePaths } from '$common/routeConfig';
-
-import { clearSearch } from '$redux/features/nav';
 
 import styles from './index.module.scss';
 
 const arrowLeftIcon = require('$assets/images/icons/arrow-left.svg');
 const arrowRightIcon = require('$assets/images/icons/arrow-right.svg');
 
+// don't forget to register plugins
+gsap.registerPlugin(Draggable);
+gsap.registerPlugin(ScrollToPlugin); // ??
+
+let v = 0;
+
 const ScrollPanel = (props) => {
   // props
   const {
     showHeader,
-    isRounded,
     title,
-    values,
+    children,
   } = props;
 
-  // store
-  const history = useHistory();
-  const dispatch = useDispatch();
-
-  //state
-  const [selected, setSelected] = useState(null);
-
   // ref
-  const arrowLeftRef = useRef(null);
-  const arrowRightRef = useRef(null);
+  const wrapperBox = useRef(null);
+  const observer = useRef(null);
+  const atStart = useRef(false);
+  const atEnd = useRef(false);
+  const firstElement = useRef(null);
+  const lastElement = useRef(null);
+
+  const callback = useCallback((entries) => {
+    atStart.current = false;
+    atEnd.current = false;
+    // console.log('entries ', entries);
+    entries.forEach((entry) => {
+      if (entry.target.className === "first-element") {
+        atStart.current = true;
+        console.log('FE');
+      }
+
+      if (entry.target.className === "last-element") {
+        atEnd.current = true;
+        console.log('LE');
+      }
+    });
+  }, []);
+
+  useEffect(() => {
+    Draggable.create("#wrapperBoxes", {
+      bounds: "#dragSpace",
+      type: "x",
+      // throwProps: true,
+      // snap: (endValue) => Math.round(endValue / 300) * 300,
+    });
+  }, []);
+
+  useEffect(() => {
+    if (!children || !children[0]) {
+      return;
+    }
+  }, [children]);
 
   // handlers
-  const handleArtistNav = (artistId) => {
-    dispatch(clearSearch());
-    history.push(generatePath(routePaths.viewArtist, { id: artistId }));
-  }
-
-  const buildMenu = () => {
-    return values.map((value, idx) => (
-      <AlbumMenu
-        key={`album-menu-${idx}`}
-        description={value.full_name}
-        url={value.avatar_url}
-        handleClick={() => handleArtistNav(value.user_id)}
-        isRounded={isRounded}
-      />
-    ));
-  };
-
-  const onSelect = (key) => {
-    setSelected(key);
-  }
-
-  const arrowLeft = (
-    <div ref={arrowLeftRef} />
-  );
-
-  const arrowRight = (
-    <div ref={arrowRightRef} />
-  );
-
   const handleNavRight = () => {
-    arrowRightRef.current.click();
+    if (atEnd.current || v < -(children.length * 300)) {
+      return;
+    }
+
+    v -= 600;
+    gsap.to(wrapperBox.current, { duration: 2, x: v });
   }
 
   const handleNavLeft = () => {
-    arrowLeftRef.current.click();
+    console.log('atStart.current ', atStart.current);
+    if (atStart.current || v > 500) {
+      return;
+    }
+
+    v += 600;
+    gsap.to(wrapperBox.current, { duration: 2, x: v });
   }
 
   // render
@@ -102,7 +113,19 @@ const ScrollPanel = (props) => {
           </div>
         )
       }
-      
+      <div id="dragSpace" className={styles.dragSpace}>
+        <div id="wrapperBoxes" ref={wrapperBox} className={`${styles.wrapperBoxes}`}>
+          <div
+            className="first-element"
+            ref={firstElement}
+          />
+          {children}
+          <div
+            className="last-element"
+            ref={lastElement}
+          />
+        </div>
+      </div>
     </div>
   );
 }
@@ -111,26 +134,24 @@ ScrollPanel.defaultProps = {
   title: '',
   showHeader: false,
   isRounded: false,
-  values: [],
+  children: null,
 };
 
 ScrollPanel.propTypes = {
   title: PropTypes.string,
   showHeader: PropTypes.bool,
   isRounded: PropTypes.bool,
-  values: PropTypes.arrayOf(PropTypes.object),
+  children: PropTypes.node,
 };
 
 export default ScrollPanel;
 
-
 /*
-<ScrollMenu
-        data={buildMenu()}
-        selected={selected}
-        onSelect={onSelect}
-        arrowLeft={arrowLeft}
-        arrowRight={arrowRight}
-        alignCenter={false}
-      />
+observer.current = new IntersectionObserver(callback, {
+      root: document.querySelector('#dragSpace'),
+      rootMargin: '0px',
+      threshold: 1.0
+    });
+    observer.current.observe(firstElement.current);
+    observer.current.observe(lastElement.current);
 */
