55c08074d2f48d6bd2689d26c26273b40d7b3321
Mark - DopestGeek <markmayalla@gmail.com>
2021-04-15 22:00:07 +0300

    Create sliderSlice and added fetchSlider async method

--- src/redux/features/slider.js
+++ src/redux/features/slider.js
@@ -1,4 +1,6 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { useSelector } from "react-redux";
+import { handleFetch } from "../../common/requestUtils";
 
 const initialState = {
     status: "idle", //the state can be one of the following values (idle, loading, successful, failed)
@@ -6,11 +8,33 @@ const initialState = {
     data: []
 }
 
+const fetchSliders = createAsyncThunk(
+    'slider/fetchSliders',
+    async (payload, store) => {
+        const { token } = store.getState().authentication
+        const response = await handleFetch('GET', 'sliders', null, token)
+    }
+)
+
 const sliderSlice = createSlice({
     name: "slider",
     initialState,
-    reducers: [],
-    extraReducers: []
+    reducers: {},
+    extraReducers: {
+        [fetchSliders.pending]: (state, action) => {
+            state.status = 'loading'
+        },
+        [fetchSliders.fulfilled]: (state, action) => {
+            state.status = 'successful'
+            state.data = action.payload
+        },
+        [fetchSliders.rejected]: (state, action) => {
+            state.status = 'failed'
+            state.error = action.error
+        }
+    }
 });
 
 export default sliderSlice.reducer
+
+export const selectAllSliders = useSelector(state => state.slider.data)
