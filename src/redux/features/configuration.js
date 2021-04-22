516f7d02d103d35d8ad81a05b17b4753b8ece1e8
Mark - DopestGeek <markmayalla@gmail.com>
2021-04-22 21:26:09 +0300

    created a method for fetching configurations

--- src/redux/features/configuration.js
+++ src/redux/features/configuration.js
@@ -1,15 +1,39 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { async } from "regenerator-runtime";
+import { handleFetch } from "../../common/requestUtils";
 
 const initialState = {
     status: 'idle',
     error: null,
     data: []
 }
+
+const fetchConfigurations = createAsyncThunk(
+    'configurations',
+    async (payload, store) => {
+        const { token } = store.getState().authentication
+        const response = await handleFetch('GET', '/configurations', null, token)
+        return response.data
+    }
+)
+
 const configurationSlice = createSlice({
     name: 'configuration',
     initialState,
     reducers: {},
-    extraReducers: {}
+    extraReducers: {
+        [fetchConfigurations.fulfilled]: (state, action) => {
+            state.status = 'done'
+            state.data = action.payload
+        },
+        [fetchConfigurations.pending]: (state, action) => {
+            state.status = 'loading'
+        },
+        [fetchConfigurations.rejected]: (state, action) => {
+            state.status = 'error'
+            state.error = action.payload
+        }
+    }
 })
 
 export default configurationSlice.reducer
