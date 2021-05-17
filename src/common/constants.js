f0f61df3f18a0829b150ef420d240c2045d0e087
Mark - DopestGeek <markmayalla@gmail.com>
2021-05-17 22:54:58 +0300

    Implemented Facebook login and signup

--- src/common/constants.js
+++ src/common/constants.js
@@ -1,2 +1,2 @@
 export const GOOGLE_CLIENT_ID = "657373538887-r4cjugt1peefmmo8b5i6bharghg90eto.apps.googleusercontent.com";
-export const FACEBOOK_APP_ID = "asdfasdfasdf";
+export const FACEBOOK_APP_ID = "1968204993311180";
