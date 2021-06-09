cc35fba172f9c3d50bc804d0b44149ade3a70f7d
Mark - DopestGeek <markmayalla@gmail.com>
2021-06-09 15:50:02 +0300

    Finalized Route Config

--- src/containers/GuestViewMedia/index.jsx
+++ src/containers/GuestViewMedia/index.jsx
@@ -1,13 +1,15 @@
 import React, { useEffect } from 'react'
 import Header from '$components/common/Header';
-import { useParams } from 'react-router-dom';
+import { useHistory, useParams } from 'react-router-dom';
 import { getMedia } from '$redux/features/media'
 import { useDispatch, useSelector } from 'react-redux';
 import VideoPlayer from '$components/media/VideoPlayer';
+import { routePaths } from '../../common/routeConfig';
 
 const GuestViewMedia = () => {
     //routes
     const { id: mediaId } = useParams();
+    const { push } = useHistory()
 
     //store
     const dispatch = useDispatch();
@@ -30,12 +32,22 @@ const GuestViewMedia = () => {
         <div>
             <div className="container">
                 <Header></Header>
+                <button onClick={() => push(routePaths.marketing)} className="btn btn-secondary mb-2">Back</button>
                 {getMediaPending ? <p>Loading...</p> : <></>}
-                <div className="row">
-                    <div className="col-md-6">
-                        {getMediaComplete ? <VideoPlayer url={currentMedia.media_url} /> : <></>}
+                {getMediaComplete ? (
+                    <div className="row">
+                        <div className="col-lg-9">
+                            <VideoPlayer url={currentMedia.media_url} />
+                            <div className="mt-3">
+                                <h2>{currentMedia.name}</h2>
+                                <div>
+                                    <span>{currentMedia.plays} Views</span>
+                                </div>
+                            </div>
+                        </div>
                     </div>
-                </div>
+                ): <></>}
+                
                 
             </div>
         </div>
