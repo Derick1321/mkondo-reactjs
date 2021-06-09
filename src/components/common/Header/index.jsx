33b7ceb28f4becbcad3afa226a2c9fc1b0fafd15
Mark - DopestGeek <markmayalla@gmail.com>
2021-06-09 15:34:37 +0300

    Enabled Guest to be able to play a video

--- src/components/common/Header/index.jsx
+++ src/components/common/Header/index.jsx
@@ -11,6 +11,20 @@ import Menu from './Menu';
 
 import styles from './index.module.scss';
 
+import styled from 'styled-components';
+const logo = require('$assets/images/logo.png');
+const Logo = styled.div`
+  background-image: url(${logo});
+  background-size: contain;
+  background-position: center;
+  background-repeat: no-repeat;
+  height: 60px;
+  width: 60px;
+  @media screen and (max-width: 768px) {
+    background-size: 45px 45px;
+  }
+`;
+
 const Header = () => {
   // state
   const [activeMenu, setActiveMenu] = useState('about');
@@ -57,7 +71,11 @@ const Header = () => {
           Sign Up
         </Button>
       </div>
-      <div className={`d-none d-sm-flex ${styles.panel}`}>
+      <div className="container">
+        <div className="row py-3">
+        <div className={`d-none d-sm-flex`}>
+        <Logo />
+        <div className="mr-auto"></div>
         <Menu
           name="about"
           title="About"
@@ -76,6 +94,8 @@ const Header = () => {
           isActive={activeMenu === 'signup'}
           onClick={handleClick}
         />
+      </div>
+        </div>
       </div>
     </div>
   );
