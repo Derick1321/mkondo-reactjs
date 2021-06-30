b014f1263c8fab9f04a30e221609ebcfd73c5aed
Mark - DopestGeek <markmayalla@gmail.com>
2021-06-30 16:40:18 +0300

    Updated the video component according to the new design

--- src/components/media/GridMedia/index.jsx
+++ src/components/media/GridMedia/index.jsx
@@ -2,7 +2,7 @@ import React, { useCallback } from 'react';
 import PropTypes from 'prop-types';
 
 import Preview from '$components/common/Preview';
-import FeatureHomeRes from '$components/common/FeatureHomeRes';
+import FeatureHome from '$components/common/FeatureHome';
 import SimplePreview from '$components/common/SimplePreview';
 import styles from './index.module.scss';
 
@@ -21,7 +21,7 @@ const GridMedia = (props) => {
     if(item.owner_avatar_url == "null") avatar_url = null;
     if (type === 'audio') {
       return (
-        <FeatureHomeRes
+        <FeatureHome
           key={`feature-home-songs-${idx}`}
           mediaUrl={item.media_url}
           mediaId={item.media_id}
@@ -53,16 +53,22 @@ const GridMedia = (props) => {
     }
 
     return (
-      <Preview
-        key={`scroll-${idx}-home-${item.name}`}
-        title={item.name}
-        mediaId={item.media_id}
-        description={item.description}
-        avatar={item.cover_url}
-        source={avatar_url}
-        likes={item.likes}
-        plays={item.plays}
-        artistId={item.owner_id}
+      <FeatureHome
+      key={`feature-home-video-${idx}`}
+      mediaUrl={item.media_url}
+      mediaId={item.media_id}
+      avatar={item.cover_url}
+      artistId={item.owner_id}
+      source={avatar_url}
+      owner_name={item.owner_name}
+      title={item.name}
+      country={item.country}
+      category={item.category}
+      description={item.description}
+
+      likes={item.likes}
+      plays={item.plays}
+      comment_num={item.comment_num}
       />
     );
   }, [type]);
