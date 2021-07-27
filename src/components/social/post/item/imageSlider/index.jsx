61c1953c851b46bc9fd20d72002aa09fe09008e6
Mark - DopestGeek <markmayalla@gmail.com>
2021-07-27 22:24:21 +0300

    Functional Post Feature

--- src/components/social/post/item/imageSlider/index.jsx
+++ src/components/social/post/item/imageSlider/index.jsx
@@ -2,29 +2,43 @@ import React, { useEffect, useState } from 'react'
 import { PropTypes } from 'prop-types';
 import { getMediaUrl } from '../../../../../common/utils';
 import { useSelector } from 'react-redux';
+import ImageSliderImage from './image';
+import { Carousel } from '../../../../common/Carousel/index';
+import styles from './index.module.scss';
 
 const PostItemImageSlider = (props) => {
     //props
     const { images } = props;
 
     //state
-    const [imageUrls, setImageUrls] = useState([]);
+    const [current, setCurrent] = useState(0);
+    const [total, setTotal] = useState(images.length);
 
-    //store
-    const token = useSelector(state => state.authentication.token);
+    //handles
+    const handleNext = () => {
+        if (current < (total - 1)) {
+            setCurrent(current + 1);
+        } else {
+            setCurrent(0);
+        }  
+    }
 
-    //effects
-    useEffect(async () => {
-        if (!images || !token) return;
-        images.map(async (filename) => {
-            const _url = await getMediaUrl(filename, token);
-            setImageUrls([...imageUrls, _url]);
-        })
-    }, [images, token]);
+    const handlePrev = () => {
+        if (current > 0) {
+            setCurrent(current - 1);
+        } else {
+            setCurrent(total - 1);
+        }  
+    }
+
 
     return (
-        <div>
-            {imageUrls.map((url, index) => <img key={url} src={url} width="100%" />)}
+        <div className={`${styles.wrapper}`}>
+            <ImageSliderImage key={images[current].url} filename={images[current].url} />
+            <div className="d-flex">
+                <button onClick={handleNext}>Next</button>
+                <button onClick={handlePrev}>Previous</button>
+            </div>
         </div>
     )
 }
