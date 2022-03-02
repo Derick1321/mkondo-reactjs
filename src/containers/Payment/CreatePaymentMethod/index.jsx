ca245fb0d3485c399b35e194f7ff85cc05f57a21
Mark - DopestGeek <markmayalla@gmail.com>
2022-03-02 16:52:59 +0300

    working on adding payment methods

--- src/containers/Payment/CreatePaymentMethod/index.jsx
+++ src/containers/Payment/CreatePaymentMethod/index.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { loadStripe } from "@stripe/stripe-js";
 import styles from './index.module.scss';
 
@@ -6,10 +6,16 @@ import styles from './index.module.scss';
 const stripePromise = loadStripe("pk_test_51KXBZaHrASyjhIVozZjo4RHNRiuKZTLg0AANL6ZyHuGLILPWvqy5jWrgHp6gEqzDd26DjBPFAvqnCmY1LtHELEkW00EFWOQuLd");
 
 export const CreatePaymentMethodContainer = () => {
+  const [secret, setSecret] = useState("")
   return (
     <div className={styles.container}>
       <h1 className={styles.title}>Add Payment Methods</h1>
 
+      <div>
+        <div>
+          <div></div>
+        </div>
+      </div>
     </div>
   )
 }
