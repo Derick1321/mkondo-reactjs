370e04144ad13720bd633625d543633379f71413
Mark - DopestGeek <markmayalla@gmail.com>
2022-03-02 08:17:59 +0300

    implemented some of the subscriptions ui

--- src/containers/Payment/CreatePaymentMethod/index.jsx
+++ src/containers/Payment/CreatePaymentMethod/index.jsx
@@ -1,7 +1,15 @@
 import React from 'react'
+import { loadStripe } from "@stripe/stripe-js";
+import styles from './index.module.scss';
+
+
+const stripePromise = loadStripe("pk_test_51KXBZaHrASyjhIVozZjo4RHNRiuKZTLg0AANL6ZyHuGLILPWvqy5jWrgHp6gEqzDd26DjBPFAvqnCmY1LtHELEkW00EFWOQuLd");
 
 export const CreatePaymentMethodContainer = () => {
   return (
-    <div>CreatePaymentMethodContainer</div>
+    <div className={styles.container}>
+      <h1 className={styles.title}>Add Payment Methods</h1>
+
+    </div>
   )
 }
