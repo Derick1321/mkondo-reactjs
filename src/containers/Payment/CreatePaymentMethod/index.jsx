affa159f6f8266bdb5eb5b2acb3839b4b2cd8fba
Mark - DopestGeek <markmayalla@gmail.com>
2022-03-02 17:21:48 +0300

    fixed the workflow

--- src/containers/Payment/CreatePaymentMethod/index.jsx
+++ src/containers/Payment/CreatePaymentMethod/index.jsx
@@ -1,12 +1,23 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { loadStripe } from "@stripe/stripe-js";
 import styles from './index.module.scss';
+import { useSelector } from 'react-redux';
 
 
 const stripePromise = loadStripe("pk_test_51KXBZaHrASyjhIVozZjo4RHNRiuKZTLg0AANL6ZyHuGLILPWvqy5jWrgHp6gEqzDd26DjBPFAvqnCmY1LtHELEkW00EFWOQuLd");
 
 export const CreatePaymentMethodContainer = () => {
+
+  //state
   const [secret, setSecret] = useState("")
+
+  //store
+  const isFetchingPaymentIntent = useSelector((store) => store.state);
+
+  //on component mount
+  useEffect(() => {
+
+  }, []);
   return (
     <div className={styles.container}>
       <h1 className={styles.title}>Add Payment Methods</h1>
