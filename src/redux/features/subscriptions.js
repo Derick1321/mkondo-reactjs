403102cddc5931ed37671afb5302adc839221b75
Mark - DopestGeek <markmayalla@gmail.com>
2022-03-04 03:05:18 +0300

    worked with adding a payment method

--- src/redux/features/subscriptions.js
+++ src/redux/features/subscriptions.js
@@ -1,4 +1,5 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit"
+import { handleFetch } from '$common/requestUtils';
 
 const initialState = {
     fetchPaymentIntentLoading: false,
@@ -15,14 +16,35 @@ const ADD_SETUP_INTENT = 'subscription/ADD_SETUP_INTENT';
 export const addSetupIntent = createAsyncThunk(
     ADD_SETUP_INTENT, 
     async (payload, store) => {
-        const { token } = store.getState().authentication;
-        return await handleFetch('POST', '/posts', payload, token);
+        const { token, user } = store.getState().authentication;
+        return await handleFetch('POST', `/users/${user.user_id}/payment-methods/setup-intent`, payload, token);
     },
 );
+
+
 
 const subscriptionSlice = createSlice({
     name: 'subscription',
     initialState,
     reducers: {},
-    extraReducers: {},
+    extraReducers: {
+        [addSetupIntent.pending]: (state, action) => {
+            state.fetchPaymentIntentLoading = true;
+            state.clientSecret = null;
+            state.fetchPaymentIntentError = null;
+        },
+        [addSetupIntent.fulfilled]: (state, action) => {
+            state.fetchPaymentIntentLoading = false;
+            state.clientSecret = action.payload.client_secret;
+            state.fetchPaymentIntentError = null;
+        },
+        [addSetupIntent.rejected]: (state, action) => {
+            state.fetchPaymentIntentLoading = false;
+            state.clientSecret = null;
+            state.fetchPaymentIntentError = "Failed to fetch payment intent";
+        }
+    },
 });
+
+export default subscriptionSlice.reducer;
+
