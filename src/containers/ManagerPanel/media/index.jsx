3ac175166bb10d08dde8e30f87cefa9bbf260e5b
Mark - DopestGeek <markmayalla@gmail.com>
2022-04-04 19:41:11 +0300

    Quick fix

--- src/containers/ManagerPanel/media/index.jsx
+++ src/containers/ManagerPanel/media/index.jsx
@@ -10,7 +10,7 @@ export const ManageMedia = () => {
 
     return (
         <div className={`${styles.container} container`}>
-            <h2 className='text-light'>Manage Movies</h2>
+            <h2 className='text-light'>Manage Media</h2>
             <div>
                 {movies.map(movie => <ManageMoviesItem key={movie.id} movie={movie} />)}
             </div>
