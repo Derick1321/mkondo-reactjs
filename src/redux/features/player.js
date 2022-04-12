c9e106ae185316632d9e6e8385b8c2001ad7a6cf
Mark - DopestGeek <markmayalla@gmail.com>
2022-04-12 13:40:20 +0300

    implemented audio player previous and next

--- src/redux/features/player.js
+++ src/redux/features/player.js
@@ -31,32 +31,34 @@ export const loadNext = createAsyncThunk(
       reject("End of playlist has reached");
       return;
     });
-
   }
 );
 
 export const loadPrevious = createAsyncThunk(
   LOAD_PREV_MEDIA,
-  async (data, param) => {
-    const { currentMediaId, isPlaying } = param.getState().player;
-    if (currentMediaId === data.mediaId) {
-      if (isPlaying) {
-        param.dispatch(pause());
-        return;
+  async (data, store) => {
+    return new Promise(async (resolve, reject) => {
+      let state = store.getState().player;
+      if (state.currentPlaylistIndex > 0) {
+        // state.currentPlaylistIndex++;
+        let _index = state.currentPlaylistIndex - 1;
+        let _media = state.currentPlaylist[_index];
+        store.dispatch(updateLoading(true));
+        store.dispatch(setCurrentMediaId(_media.mediaId));
+
+        let { token, visitorToken } = store.getState().authentication;
+        if (!token) token = visitorToken;
+
+        const res = await handleFetch('GET', `media/presigned-get-url?file_name=${_media.url}`, null, token);
+        store.dispatch(play({
+          ..._media,
+          url: res.response,
+        }));
+        resolve(_index);
       }
-      param.dispatch(play());
+      reject("End of playlist has reached");
       return;
-    }
-    param.dispatch(updateLoading(true));
-    param.dispatch(setCurrentMediaId(data.mediaId));
-    //populate the queue, by loading the required medias
-    let { token, visitorToken } = param.getState().authentication;
-    if(!token) {token = visitorToken;}
-    const res = await handleFetch('GET', `media/presigned-get-url?file_name=${data.url}`, null, token);
-    param.dispatch(play({
-      ...data,
-      url: res.response,
-    }));
+    });
   }
 );
 
@@ -92,6 +94,13 @@ const playerSlider = createSlice({
     play(state, action) {
       if (action.payload) {
         state.currentMediaId = action.payload.mediaId; // TODO: media id fix
+
+        if (state.currentPlaylist.some((media) => media.mediaId == action.payload.mediaId)) {
+          let _index = state.currentPlaylist.findIndex((media) => media.mediaId == action.payload.mediaId);
+          state.currentPlaylist[_index] = action.payload;
+        } else {
+          state.currentPlaylist.unshift(action.payload);
+        }
       }
       state.isPlaying = true;
     },
@@ -126,6 +135,9 @@ const playerSlider = createSlice({
   extraReducers: {
     [loadNext.fulfilled]: (state, action) => {
       state.currentPlaylistIndex = action.payload;
+    },
+    [loadPrevious.fulfilled]: (state, action) => {
+      state.currentPlaylistIndex = action.payload
     }
   }
 });
