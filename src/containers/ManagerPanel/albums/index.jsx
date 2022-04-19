ec50bd50fb127680b0a434e7c368732b0cb79ee5
Mark - DopestGeek <markmayalla@gmail.com>
2022-04-19 14:30:48 +0300

    finalized media panel

--- src/containers/ManagerPanel/albums/index.jsx
+++ src/containers/ManagerPanel/albums/index.jsx
@@ -5,7 +5,6 @@ import { fetchAlbums } from '../../../redux/features/media';
 import { ManageAlbumsItem } from './item';
 
 export const ManageAlbum = () => {
-
   //store
   const dispatch = useDispatch();
   const albums = useSelector((state) => state.media.albums);
@@ -22,8 +21,8 @@ export const ManageAlbum = () => {
   return (
     <div className={`${styles.container} container`}>
         <h2 className='text-light'>Manage Albums</h2>
-        <div className='d-flex'>
-            {albums.map(album => <ManageAlbumsItem key={album.album_id} album={album} />)}
+        <div className='row'>
+            {albums.map(album => <div className='col-lg-2 col-md-4 col-6'><ManageAlbumsItem key={album.album_id} album={album} /></div>)}
         </div>
     </div>
   )
--- src/containers/ManagerPanel/series/index.jsx
+++ src/containers/ManagerPanel/series/index.jsx
@@ -1,12 +1,28 @@
-import React from 'react';
+import React, { useEffect } from 'react'
 import styles from './index.module.scss';
+import { useSelector, useDispatch } from 'react-redux';
+import { fetchAlbums, getSeries } from '../../../redux/features/media';
+import { ManageAlbumsItem, ManageSeriesItem } from './item';
 
 export const ManageSeries = () => {
+  //store
+  const dispatch = useDispatch();
+  const series = useSelector((state) => state.media.mySeries);
+  const isFetchingSeries = useSelector((state) => state.media.getSeriesPending);
+
+  //effects
+  useEffect(() => {
+    if (!series.length) {
+      console.log("dispatching fetching series action");
+      dispatch(getSeries());
+    }
+  }, []);
+
   return (
     <div className={`${styles.container} container`}>
         <h2 className='text-light'>Manage Series</h2>
-        <div>
-            {/* {media.map(_media => <ManageMediaItem key={_media.id} media={_media} />)} */}
+        <div className='row'>
+            {series.map(x => <div className='col-lg-2 col-md-4 col-6'><ManageSeriesItem key={x.series_id} series={x} /></div>)}
         </div>
     </div>
   )
