a20c4f42570de0a8cb2a37385426cc51d5eda1de
Mark - DopestGeek <markmayalla@gmail.com>
2022-04-30 19:30:00 +0300

    Implemented a dashboard in a home page

--- src/containers/Configuration/Sliders/index.jsx
+++ src/containers/Configuration/Sliders/index.jsx
@@ -2,12 +2,21 @@ import React from 'react'
 import { Form } from './form'
 import styles from './index.module.scss'
 
+export const CONFIG_KEY_SLIDER_HOME = "HOME_SLIDER";
+export const CONFIG_KEY_SLIDER_DASHBOARD = "DASHBOARD_SLIDER";
+
 export const Sliders = () => {
     return (
         <div className={`${styles.container}`}>
             <h3>Slider Settings</h3>
             
-            <Form />
+            <div className={styles.tile}>
+                <Form configKey={CONFIG_KEY_SLIDER_HOME} />
+            </div>
+
+            <div className={styles.tile}>
+                <Form configKey={CONFIG_KEY_SLIDER_DASHBOARD} />
+            </div>
         </div>
     )
 }
