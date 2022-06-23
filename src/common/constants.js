a2bd6dcde6bc1e42a9563c59d89f99dc61506af9
Mark - DopestGeek <markmayalla@gmail.com>
2022-06-23 17:48:54 +0300

    fixed google login error

--- src/common/constants.js
+++ src/common/constants.js
@@ -1,4 +1,4 @@
-export const GOOGLE_CLIENT_ID = "657373538887-r4cjugt1peefmmo8b5i6bharghg90eto.apps.googleusercontent.com";
+export const GOOGLE_CLIENT_ID = "834112317612-fohhfhaenpbm1vrnk5ir0jfaoh8i6h94.apps.googleusercontent.com";
 export const FACEBOOK_APP_ID = "1968204993311180";
 
 export const COLOR_PRIMARY = '#8103FC';
