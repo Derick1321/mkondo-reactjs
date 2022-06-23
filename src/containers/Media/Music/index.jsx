0f9613554eb36824558a7409b35e78c4164bfc4b
Mark - DopestGeek <markmayalla@gmail.com>
2022-06-23 21:05:09 +0300

    getting there, trying to display albums on top of the scroller

--- src/containers/Media/Music/index.jsx
+++ src/containers/Media/Music/index.jsx
@@ -3,17 +3,20 @@ import styles from './index.module.scss';
 import { getPlaylist } from '../../../redux/features/playlist';
 import { MusicPlaylistComponent } from './widgets/playlist';
 import { useDispatch, useSelector } from 'react-redux';
-import { getNewReleases, getTopMedias, getTrendMedias } from '../../../redux/features/media';
+import { getNewReleases, getTopMedias, getTrendMedias, getNewAlbums } from '../../../redux/features/media';
 import CarouselFromConfiguration from '../../../components/common/Carousel/carousel_from_configuration';
 import { CONFIG_KEY_SLIDER_DASHBOARD } from '../../Configuration/Sliders';
 import fireIcon from '$assets/images/icons/fire.svg';
 import newTag from '$assets/images/icons/new-tag.svg';
+import Scroller from '../../../components/common/Scroller/index';
+import { FeatureAlbum } from '../../../components/common/FeatureAlbum';
 
 export const MusicContainer = () => {
     //store
     const dispatch = useDispatch();
     const topSongs = useSelector(state => state.media.topMedias.audio);
     const newSongs = useSelector(state => state.media.newReleases.audio);
+    const newAlbums = useSelector(state => state.media.newReleases.albums);
     const trendingSongs = useSelector(state => state.media.trendMedias.audio);
     const favouriteSongs = useSelector(state => state.authentication.user.favourites.filter(item => item.category == 'audio'));
 
@@ -28,6 +31,9 @@ export const MusicContainer = () => {
         }
         if (!trendingSongs.lenght) {
             dispatch(getTrendMedias(params));
+        }
+        if (!newAlbums.lenght) {
+            dispatch(getNewAlbums());
         }
     }, [])
 
@@ -44,6 +50,19 @@ export const MusicContainer = () => {
                 <MusicPlaylistComponent title="New Songs" icon={newTag} media={newSongs} />
                 <div className="mt-5"></div>
                 {favouriteSongs.length > -1 ? <MusicPlaylistComponent title="Favourites" icon={fireIcon} media={favouriteSongs} /> : null}
+
+                <Scroller
+                    isLoading={false}
+                    title="New Albums"
+                    showHeader={true}
+                    total={newAlbums.length}
+                    // name={name}
+                    // viewMore={viewMore}
+                    >
+                    {
+                        newAlbums.map((album, i) => <FeatureAlbum key={`new-album-${i}`} album={album} />)
+                    }
+                </Scroller>
             </div>
 
         </div>
