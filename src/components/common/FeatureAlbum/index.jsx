76d3adab3265401e5324c91efef1192ffc0f7883
Mark - DopestGeek <markmayalla@gmail.com>
2022-06-23 21:05:09 +0300

    getting there, trying to display albums on top of the scroller

---
+++ src/components/common/FeatureAlbum/index.jsx
@@ -0,0 +1,20 @@
+import React, { useState } from 'react'
+import styles from './index.module.scss';
+
+export const FeatureAlbum = (props) => {
+    //props
+    const { album } = props;
+
+    //state
+    const [cover, setCover] = useState();
+
+    //effects
+
+    //handlers
+    const handlePlay = () => {}
+    const handleOpen = () => {}
+
+    return (
+        <div>Feature Album</div>
+    );
+}
