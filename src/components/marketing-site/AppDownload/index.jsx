187800e74f47b281d6494449cd6687fabcd0ea99
Mark - DopestGeek <markmayalla@gmail.com>
2022-06-27 19:29:51 +0300

    Refactoring Homepage

--- src/components/marketing-site/AppDownload/index.jsx
+++ src/components/marketing-site/AppDownload/index.jsx
@@ -9,7 +9,7 @@ const playstore = require('$assets/images/playstore.svg');
 const AppDownload = () => {
   return (
     <div className="row">
-      <div className="col-12 col-md-3">
+      <div className="d-none d-md-block col-12 col-md-3">
         <img
           src={phone}
           className={styles.phoneMkImage}
@@ -21,16 +21,20 @@ const AppDownload = () => {
           <p className={`${styles.appDownloadHeader}`}>Never stop listening</p>
           <p>Discover, stream, and share a constantly expanding mix of music from emerging and major artists around the world.</p>
           <div className="d-flex">
-            <img
-              src={appstore}
-              className={styles.storeIcon}
-              alt=""
-            />
-            <img
-              src={playstore}
-              className={styles.storeIcon}
-              alt=""
-            />
+            <a href="https://play.google.com/store/apps/details?id=co.mkondo.mkondo.mkondo_app_new" target="_blank" rel="noopener noreferrer">
+              <img
+                src={appstore}
+                className={styles.storeIcon}
+                alt=""
+              />
+            </a>
+            <a href="" target="_blank" rel="noopener noreferrer">
+              <img
+                src={playstore}
+                className={styles.storeIcon}
+                alt=""
+              />
+            </a>
           </div>
         </div>
         <div />
