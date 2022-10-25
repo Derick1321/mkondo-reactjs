840fa31166cb970cae74b0c9d48394833e634030
Mark - DopestGeek <markmayalla@gmail.com>
2022-10-25 11:23:02 +0300

    perfected the player somehow

---
+++ src/components/icons/next.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const IconNext = (props) => {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill='currentColor' {...props}>
+        <g>
+            <path d="M3 19c-.2 0-.3 0-.5-.1 -.3-.2-.5-.5-.5-.9V6c0-.4.2-.7.5-.9 .3-.2.7-.2 1 0l9 6c.3.2.4.5.4.8 0 .3-.2.6-.4.8l-9 6c-.1.2-.3.3-.5.3ZM4 7.9v8.3l6.2-4.1L4 7.9Z"></path>
+            <path d="M12 19c-.2 0-.3 0-.5-.1 -.3-.2-.5-.5-.5-.9V6c0-.4.2-.7.5-.9 .3-.2.7-.2 1 0l9 6c.3.2.4.5.4.8 0 .3-.2.6-.4.8l-9 6c-.1.2-.3.3-.5.3Zm1-11.1v8.3l6.2-4.1L13 7.9Z"></path>
+        </g>
+    </svg>
+  )
+}
---
+++ src/components/icons/pause.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const IconPause = (props) => {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 28" fill="currentColor" {...props}>
+        <path fillRule="evenodd" d="M490 571h-4c-1.11 0-2 .89-2 2v24c0 1.1.89 2 2 2h4c1.1 0 2-.9 2-2v-24c0-1.11-.9-2-2-2v0Zm-12 0h-4c-1.11 0-2 .89-2 2v24c0 1.1.89 2 2 2h4c1.1 0 2-.9 2-2v-24c0-1.11-.9-2-2-2v0Z" transform="translate(-472-571)"></path>
+    </svg>
+  )
+}
