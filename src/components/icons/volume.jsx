7596cc420b0aab0833999492649e22debe84a920
Mark - DopestGeek <markmayalla@gmail.com>
2022-10-25 11:23:02 +0300

    perfected the player somehow

---
+++ src/components/icons/volume.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const IconVolume = (props) => {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill='currentColor' {...props}>
+        <g>
+            <path d="M16 21c3.52-1.55 5.99-4.91 5.99-9 0-4.1-2.48-7.453-6-9v2c2.38 1.38 3.99 4.04 3.99 7 0 2.95-1.62 5.61-4 7v2Z"></path>
+            <path d="M16 7v10c1.22-1.1 2-3.23 2-5 0-1.78-.78-3.9-2-5ZM4 17h2.697L14 21.86V2.12L6.69 6.98h-2.7c-1.11 0-2 .89-2 2v6c0 1.1.89 2 2 2Z"></path>
+        </g>
+    </svg>
+  )
+}
