3aaa75ae60467c9a011f7f8c862f63dfbd31253e
Mark - DopestGeek <markmayalla@gmail.com>
2022-10-25 13:34:10 +0300

    implemented filters for music and video

---
+++ src/components/icons/sort.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const IconSort = (props) => {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill='currentColor' {...props}>
+        <path d="M8 16H4l6 6v-3 -3V2H8Zm6-11v3 14h2V8h4l-6-6Z"></path>
+    </svg>
+  )
+}
