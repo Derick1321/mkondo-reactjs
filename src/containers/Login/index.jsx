329367db655189dbc4392129395486426fd5022c
Mark - DopestGeek <markmayalla@gmail.com>
2022-11-16 11:46:49 +0300

    UI/UX for login and registration pages

--- src/containers/Login/index.jsx
+++ src/containers/Login/index.jsx
@@ -1,32 +1,32 @@
-import React, { useState, useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { useHistory } from 'react-router-dom';
-import styled from 'styled-components';
+import React, { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useHistory } from "react-router-dom";
+import styled from "styled-components";
 
-import Button from '$components/common/Button';
-import TextInput from '$components/common/TextInput';
-import InfoPane from '$components/authentication/Info';
-import Alert from '$components/authentication/Alert';
-import GoogleLoginComponent from '$components/modals/GoogleLoginComponent';
-import FacebookLoginComponent from '$components/modals/FacebookLoginComponent';
+import Button from "$components/common/Button";
+import TextInput from "$components/common/TextInput";
+import InfoPane from "$components/authentication/Info";
+import Alert from "$components/authentication/Alert";
+import GoogleLoginComponent from "$components/modals/GoogleLoginComponent";
+import FacebookLoginComponent from "$components/modals/FacebookLoginComponent";
 
-import { showModal, hideModal } from '$redux/features/modal';
-import { login } from '$redux/features/authentication';
+import { showModal, hideModal } from "$redux/features/modal";
+import { login } from "$redux/features/authentication";
 
-import { routePaths } from '$common/routeConfig';
+import { routePaths } from "$common/routeConfig";
 
-import './index.module.scss';
-import { COLOR_ACCENT, COLOR_PRIMARY } from '$common/constants';
-import styles from './index.module.scss';
-import MkondoLogo from '../../components/common/logo';
-import { visitorColdStart } from '../../redux/features/authentication';
+import "./index.module.scss";
+import { COLOR_ACCENT, COLOR_PRIMARY } from "$common/constants";
+import styles from "./index.module.scss";
+import MkondoLogo from "../../components/common/logo";
+import { visitorColdStart } from "../../redux/features/authentication";
+import two from "$assets/images/graphics/two.jpg";
 
-const login_banner_top_hd = require('$assets/images/banner-login-top.png');
-const login_mobile_top = require('$assets/images/login_mobile_top.png');
-const login_mobile_bottom = require('$assets/images/login_mobile_bottom.png');
-const logo = require('$assets/images/logo.png');
-const arrow_left = require('$assets/images/icons/arrow-left-home.svg');
-
+const login_banner_top_hd = require("$assets/images/banner-login-top.png");
+const login_mobile_top = require("$assets/images/login_mobile_top.png");
+const login_mobile_bottom = require("$assets/images/login_mobile_bottom.png");
+const logo = require("$assets/images/logo.png");
+const arrow_left = require("$assets/images/icons/arrow-left-home.svg");
 
 const GradientBackground = styled.div`
   width: 100%;
@@ -67,8 +67,8 @@ const LoginBack = styled.div`
 `;
 
 const initialValues = {
-  email: '',
-  password: '',
+  email: "",
+  password: "",
 };
 
 const LoginPage = () => {
@@ -78,7 +78,9 @@ const LoginPage = () => {
   // store
   const dispatch = useDispatch();
   const history = useHistory();
-  const loginPending = useSelector((store) => store.authentication.loginPending);
+  const loginPending = useSelector(
+    (store) => store.authentication.loginPending
+  );
   const error = useSelector((store) => store.authentication.loginError);
   const token = useSelector((store) => store.authentication.token);
   const user = useSelector((store) => store.authentication.user);
@@ -87,7 +89,7 @@ const LoginPage = () => {
   useEffect(() => {
     dispatch(visitorColdStart());
   }, []);
-  
+
   useEffect(() => {
     if (token) {
       if (!user.email || !user.phone) {
@@ -107,88 +109,88 @@ const LoginPage = () => {
     });
   };
 
-  const handleForgotPassword = () => { dispatch(showModal('FORGOT_PASSWORD_MODAL')); };
+  const handleForgotPassword = () => {
+    dispatch(showModal("FORGOT_PASSWORD_MODAL"));
+  };
 
   const handleSignUp = () => history.push(routePaths.register);
 
   const handleSignIn = () => {
     const { email, password } = values;
-    dispatch(login({
-      login_strategy: 'local',
-      username: email,
-      password,
-      tokenId: null
-    }));
+    dispatch(
+      login({
+        login_strategy: "local",
+        username: email,
+        password,
+        tokenId: null,
+      })
+    );
   };
 
   // render
   return (
     <div className={`${styles.background}`}>
+      <div className="row">
       <div className="col-lg-6">
-        <div className="px-3 py-4">
+        <img src={two} alt="" width="100%" />
+      </div>
+      <div className="col-lg-6">
+        {/* <div className="px-3 py-4">
           <MkondoLogo />
-        </div>
+        </div> */}
         <div className="d-flex flex-column h-100 px-4">
           <span className="font-bold text-light display-2">Sign In</span>
-              {
-                error && (
-                  <Alert
-                    content={error}
-                    type="error"
-                  />
-                )
-              }
-              <TextInput
-                name="email"
-                placeholder="Email Address / User Name"
-                value={values.email}
-                isGrey={true}
-                onChange={handleChange}
-              />
-              <TextInput
-                name="password"
-                placeholder="Password"
-                type="password"
-                value={values.password}
-                isGrey={true}
-                onChange={handleChange}
-              />
-              <div className="d-flex align-items-center my-2 mt-4">
-                <button
-                  onClick={handleSignIn}
-                  isLoading={loginPending}
-                  className="btn btn-primary mr-2"
-                >
-                  {loginPending ? <span className="spinner-border w20"></span> : <span>LOGIN</span>}
-                </button>
-                <button
-                  onClick={handleSignUp}
-                  className="btn btn-outline-primary"
-                >
-                  REGISTER
-                </button>
-                <button
-                  className="btn btn-link ml-auto"
-                  onClick={handleForgotPassword}
-                >
-                  Forgot Password?
-                </button>
-              </div>
-
-              <div className="mt-4">
-                <GoogleLoginComponent />
-              </div>
-              <div className="my-2">
-                <FacebookLoginComponent />
-              </div>
-              <div className="d-flex justify-content-center my-2">
-
-              </div>
+          {error && <Alert content={error} type="error" />}
+          <TextInput
+            name="email"
+            placeholder="Email Address / User Name"
+            value={values.email}
+            isGrey={true}
+            onChange={handleChange}
+          />
+          <TextInput
+            name="password"
+            placeholder="Password"
+            type="password"
+            value={values.password}
+            isGrey={true}
+            onChange={handleChange}
+          />
+          <div className="d-flex align-items-center my-2 mt-4">
+            <button
+              onClick={handleSignIn}
+              isLoading={loginPending}
+              className="btn btn-primary mr-2"
+            >
+              {loginPending ? (
+                <span className="spinner-border w20"></span>
+              ) : (
+                <span>LOGIN</span>
+              )}
+            </button>
+            <button onClick={handleSignUp} className="btn btn-outline-primary">
+              REGISTER
+            </button>
+            <button
+              className="btn btn-link ml-auto"
+              onClick={handleForgotPassword}
+            >
+              Forgot Password?
+            </button>
           </div>
+
+          <div className="mt-4">
+            <GoogleLoginComponent />
+          </div>
+          <div className="my-2">
+            <FacebookLoginComponent />
+          </div>
+          <div className="d-flex justify-content-center my-2"></div>
         </div>
-    
+      </div>
+      </div>
     </div>
   );
-}
+};
 
 export default LoginPage;
