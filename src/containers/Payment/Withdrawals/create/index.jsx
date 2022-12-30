4bcadce2acba1195154558f1eb9f0c827ab0977f
uchafutz <uchafutz@gmail.com>
2022-12-30 14:19:06 +0300

    update on form

--- src/containers/Payment/Withdrawals/create/index.jsx
+++ src/containers/Payment/Withdrawals/create/index.jsx
@@ -4,6 +4,8 @@ import * as styles from "./index.module.scss";
 import { useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { createWithdrawRequest } from "../../../../redux/features/withdraw";
+import { useEffect } from "react";
+import InputField from "../../../../components/forms/InputField";
 
 const initialValues = {
   Amount: "",
@@ -17,9 +19,12 @@ export const WithdrawalsCreate = () => {
   const { goBack } = useHistory();
 
   // state
+  const [mounted, setMounted] = useState(false);
   const [isActiveButton, setIsActiveButton] = useState(false);
-  const [isLoading, setILoading] = useState(false);
+  const [isShowAlert, setShowAlert] = useState(false);
+  const [alertMessage, setAlertMessage] = useState("");
   const [isValue, setValue] = useState(initialValues);
+  const [validationErrors, setValidationErrors] = useState({});
 
   // redux
   const dispatch = useDispatch();
@@ -30,35 +35,62 @@ export const WithdrawalsCreate = () => {
   const createWithdrawal = useSelector((state) => state.withdraw.createWithdrawals);
 
   // effects
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
   React.useEffect(() => {
     if (createWithdrawal.isSuccess) {
       goBack();
     }
   }, [createWithdrawal.isSuccess]);
 
+  useEffect(() => {
+    if (!mounted) return;
+    if (!isShowAlert) return
+    //console.log("showing the alert");
+    setTimeout(() => {
+      console.log("hidding the alert");
+      setShowAlert(false);
+      setAlertMessage("");
+    }, 5000);
+  }, [isShowAlert])
+
   // handle create withdrawal error
   React.useEffect(() => {
+    if (!mounted) return;
     if (createWithdrawal.error) {
       try {
         let json = JSON.parse(createWithdrawal.error.message);
+        console.log("the error object", json);
         // check if json message is string
         if (typeof json.message === "string") {
-          alert(json.message);
+          // alert(json.message);
+          setAlertMessage(json.message);
+          setShowAlert(true);
         }
 
         // check if json message is array
         if (Array.isArray(json.message)) {
-          alert(JSON.stringify(json.message));
+          setAlertMessage(json.message);
+          setShowAlert(true);
+          // alert(JSON.stringify(json.message));
         }
 
         // check if json message is object
         if (typeof json.message === "object") {
           // stringify
-          alert(JSON.stringify(json.message));
+          //  alert(JSON.stringify(json.message));
+          setAlertMessage("Validation errors");
+          setValidationErrors(json.message);
+          setShowAlert(true);
         }
-      
+
+        // setAlertMessage("Something went wrong..!");
       } catch (error) {
-        alert("Something went wrong");
+
+        setAlertMessage("Something went wrong..!");
+        setShowAlert(true);
       }
     }
   }, [createWithdrawal.error]);
@@ -70,6 +102,10 @@ export const WithdrawalsCreate = () => {
     const value = event.target.value;
     setValue((values) => ({ ...values, [name]: value }));
   };
+
+  const handleChangeV2 = (name, value) => {
+    setValue((values) => ({ ...values, [name]: value }));
+  }
 
   const handleSubmitData = () => {
     const { Amount, BankType, AccountNo, AccountName } = isValue;
@@ -93,8 +129,24 @@ export const WithdrawalsCreate = () => {
       <div className="row">
         <div className="row">
           <div className="col-md-8">
+            <div className="container">
+              {isShowAlert ? <div className="alert alert-danger">
+                {/* <a href="#" className="close" dataDismiss="alert" ariaLabel="close">&times;</a> */}
+                <strong>{alertMessage}</strong>
+              </div> : null}
+            </div>
             <div className="mb-3">
               <label className="form-label text-light">Amount</label>
+              <InputField
+                field={{
+                  "type": "text",
+                  "name": "amount",
+                  "value": isValue.amount,
+                  "placeholder": "Amount"
+                }}
+                error={validationErrors.amount}
+                onChange={handleChangeV2}
+              />
               <input
                 type="number"
                 name="Amount"
