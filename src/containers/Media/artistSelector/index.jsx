72679e3c262e36e6ecdeac3c07f3456f75070575
uchafutz <uchafutz@gmail.com>
2023-09-30 14:56:04 +0300

    update on artist search

--- src/containers/Media/artistSelector/index.jsx
+++ src/containers/Media/artistSelector/index.jsx
@@ -6,6 +6,8 @@ import { useSelector, useDispatch } from 'react-redux';
 import { getArtists } from '../../../redux/features/artist';
 import { useEffect } from 'react';
 
+
+
 const ArtistSelectorComponent = (props) => {
     const { onArtistSelected } = props;
 
@@ -15,30 +17,57 @@ const ArtistSelectorComponent = (props) => {
     const artists = useSelector(state => state.artist.artists);
     const isFetchingArtist = useSelector(state => state.artist.getArtistsPending);
 
+    const [searchQuery, setSearchQuery] = useState('');
+    const [filteredArtists, setFilteredArtists] = useState([]);
+
+    const handleSearch = () => {
+
+        const filteredArtists = artists.filter((artist) =>
+            artist.full_name.toLowerCase().includes(searchQuery.toLowerCase())
+        );
+
+        setFilteredArtists(filteredArtists);
+    };
+
     useEffect(() => {
         if (!artists.length) {
             dispatch(getArtists());
         }
     }, []);
 
+
     return (
         <div className={`row`}>
             <div className={`card ${styles.card}`}>
                 <div className="card-body">
-                <h3 className="card-title text-white">Select an Artist</h3>
+                    <h3 className="card-title text-white">Select an Artist </h3>
 
-                <div className="row text-light">
-                    <div className="mb-2 d-flex align-items-center bg-dark">
-                        <ArtistListArtistWidget artist={{ ...user, full_name: "Me" }} />
-                        <button className="btn btn-primary btn-xs ml-auto" onClick={() => onArtistSelected(user)}>Select</button>
+                    {/* Search input field */}
+                    <div className="mb-3">
+                        <input
+                            type="text"
+                            className="form-control"
+                            placeholder="Search artist"
+                            value={searchQuery}
+                            onChange={(e) => setSearchQuery(e.target.value)}
+                        />
+                        <button className="btn btn-primary" onClick={handleSearch}>
+                            Search
+                        </button>
                     </div>
-                    {isFetchingArtist && <p>Fetching artists... <span className='spinner-border text-light'></span></p>}
-                    {artists.filter(artist => user.user_type == 'super admin' ? true : artist.admin_id == user.user_id).map(artist => (
-                        <div key={artist.user_id} className='mb-2 d-flex align-items-center bg-dark'>
-                            <ArtistListArtistWidget artist={artist} />
-                            <button className="btn btn-primary btn-xs ml-auto" onClick={() => onArtistSelected(artist)}>Select</button>
-                        </div>
-                    ))}
+
+                    <div className="row text-light">
+                        {/* Display filtered artists */}
+                        {filteredArtists.length > 0 ? (
+                            filteredArtists.map((artist) => (
+                                <div key={artist.user_id} className='mb-2 d-flex align-items-center bg-dark'>
+                                    <ArtistListArtistWidget artist={artist} />
+                                    <button className="btn btn-primary btn-xs ml-auto" onClick={() => onArtistSelected(artist)}>Select</button>
+                                </div>
+                            ))
+                        ) : (
+                            <p>No artists found.</p>
+                        )}
                     </div>
                 </div>
             </div>
